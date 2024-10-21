485869269f8a9a73f0c4d7f9b5c74b74c733ca14
AlyaZahra <alyazahs13@gmail.com>
2024-10-21 10:28:57 +0700

    database

--- app/src/main/java/com/project/listugas/database/ListDatabase.kt
+++ app/src/main/java/com/project/listugas/database/ListDatabase.kt
@@ -1,4 +1,36 @@
 package com.project.listugas.database
 
-class ListDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.project.listugas.dao.MatkulDao
+import com.project.listugas.dao.NoteDao
+import com.project.listugas.dao.TugasDao
+import com.project.listugas.entity.Matkul
+import com.project.listugas.entity.Note
+import com.project.listugas.entity.Tugas
+
+@Database(entities = [Matkul::class, Tugas::class, Note::class], version = 2, exportSchema = false)
+abstract class ListDatabase : RoomDatabase() {
+    abstract fun matkulDao(): MatkulDao
+    abstract fun tugasDao(): TugasDao
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ListDatabase? = null
+
+        fun getDatabase(context: Context): ListDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ListDatabase::class.java,
+                    "todo_database"
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
