334614e761b2632d479d8534169e8aaa55339d18
AlyaZahra <alyazahs13@gmail.com>
2024-10-21 13:04:13 +0700

    list kt

--- app/src/main/java/com/project/listugas/ListActivity.kt
+++ app/src/main/java/com/project/listugas/ListActivity.kt
@@ -1,4 +1,32 @@
 package com.project.listugas
 
-class ListActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.project.ListTugas.databinding.ActivityListBinding
+
+class ListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityListBinding
+    private var matkulId: Int = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        matkulId = intent.getIntExtra("MATKUL_ID", -1)
+
+        binding.iconNote.setOnClickListener {
+            val intent = Intent(this, NoteActivity::class.java)
+            intent.putExtra("MATKUL_ID", matkulId)
+            startActivity(intent)
+        }
+
+        binding.iconTodo.setOnClickListener {
+            val intent = Intent(this, TugasActivity::class.java)
+            intent.putExtra("MATKUL_ID", matkulId)
+            startActivity(intent)
+        }
+    }
 }
