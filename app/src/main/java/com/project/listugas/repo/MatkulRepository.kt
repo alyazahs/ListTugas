dfcfe5b75478dbeb82b8c40f169e43c925d502f5
Erzal74 <erzalabi@gmail.com>
2024-10-24 12:44:11 +0700

    MatkulRepository

--- app/src/main/java/com/project/listugas/repo/MatkulRepository.kt
+++ app/src/main/java/com/project/listugas/repo/MatkulRepository.kt
@@ -6,27 +6,22 @@ import com.project.listugas.entity.Matkul
 
 class MatkulRepository(private val matkulDao: MatkulDao) {
 
-    // Mengambil semua data mata kuliah
     fun getAllMatkuls(): LiveData<List<Matkul>> {
         return matkulDao.getAllMatkuls()
     }
 
-    // Mengambil data mata kuliah berdasarkan ID
     fun getMatkulById(matkulId: Int): LiveData<Matkul> {
         return matkulDao.getMatkulById(matkulId)
     }
 
-    // Menyisipkan data mata kuliah baru
     suspend fun insert(matkul: Matkul) {
         matkulDao.insert(matkul)
     }
 
-    // Memperbarui data mata kuliah yang ada
     suspend fun update(matkul: Matkul) {
         matkulDao.update(matkul)
     }
 
-    // Menghapus data mata kuliah
     suspend fun delete(matkul: Matkul) {
         matkulDao.delete(matkul)
     }
